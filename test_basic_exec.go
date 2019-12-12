c79f9f6333a1304c51c416dd1e33ef1f3a01ad8f
Paul Kuruvilla <rohitpaulk@gmail.com>
2019-12-12 17:06:47 +0530

    Move assertions to a common file

--- test_basic_exec.go
+++ test_basic_exec.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func testBasicExec(executable *Executable, logger *customLogger) error {
 	logger.Debugf("Executing 'echo foo'")
 	result, err := executable.Run("run", "alpine", "echo", "foo")
@@ -44,33 +39,6 @@ func testBasicExec(executable *Executable, logger *customLogger) error {
 
 	if err = assertExitCode(result, 2); err != nil {
 		return err
-	}
-
-	return nil
-}
-
-func assertStdout(result ExecutableResult, expected string) error {
-	actual := string(result.Stdout)
-	if expected != actual {
-		return fmt.Errorf("Expected %q as stdout, got: %q", expected, actual)
-	}
-
-	return nil
-}
-
-func assertStderrContains(result ExecutableResult, expectedSubstring string) error {
-	actual := string(result.Stderr)
-	if !strings.Contains(actual, expectedSubstring) {
-		return fmt.Errorf("Expected stderr to contain %q, got: %q", expectedSubstring, actual)
-	}
-
-	return nil
-}
-
-func assertExitCode(result ExecutableResult, expected int) error {
-	actual := result.ExitCode
-	if expected != actual {
-		return fmt.Errorf("Expected %d as exit code, got: %d", expected, actual)
 	}
 
 	return nil
